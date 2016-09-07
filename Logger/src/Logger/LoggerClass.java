192a6de096852c9e8eee2c3f4840a279336e3bc1
lukeiscoding <lukeiscoding@users.noreply.github.com>
2016-09-06 21:17:02 -0700

    LoggerClass with windowbuilder
    Lukeiscoding master piece

---
+++ Logger/src/Logger/LoggerClass.java
@@ -0,0 +1,43 @@
+package Logger;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LoggerClass {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoggerClass window = new LoggerClass();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoggerClass() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
